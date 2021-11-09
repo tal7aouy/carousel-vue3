cbf8a239e170df3fa59648f4dcee799c753465c8
tal7aouy <talhaouycoding@gmail.com>
2021-11-09 20:15:54 +0100

    add currentSlide

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,11 +1,16 @@
 <script>
+import { ref } from 'vue'
 export default {
   name: 'Carousel',
+  setup() {
+    const currentSlide = ref(1)
+    return { currentSlide }
+  },
 }
 </script>
 
 <template>
   <div class="carousel">
-    <slot />
+    <slot :currentSlide="currentSlide" />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <Carousel class="carousel">
-      <Slide>
-        <div>
-          <h2>Hello slider</h2>
+    <Carousel class="carousel" v-slot="{ currentSlide }">
+      <Slide v-for="(slide, index) in carouselSlides" :key="index">
+        <div v-show="currentSlide === index + 1" class="slide-info">
+          <img :src="slide" alt="" />
         </div>
       </Slide>
     </Carousel>
@@ -19,12 +19,38 @@ export default {
     Carousel,
     Slide,
   },
+  setup() {
+    const carouselSlides = [
+      '/images/1.jpg',
+      '/images/2.jpg',
+      '/images/3.jpg',
+      '/images/4.jpg',
+    ]
+    return {
+      carouselSlides,
+    }
+  },
 }
 </script>
 
 <style scoped>
 .carousel {
+  position: relative;
   max-height: 100vh;
   height: 100vh;
+}
+.slide-info {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  max-height: 100%;
+  height: 100%;
+}
+img {
+  min-width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 </style>
