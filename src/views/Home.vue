c00105f32fdb1d9f6e97ef925f26bcdbb382aac4
tal7aouy <talhaouycoding@gmail.com>
2021-11-09 19:57:08 +0100

    setup carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="home">
-
+  <div>
+    <Carousel class="carousel">
+      <Slide>
+        <div>
+          <h2>Hello slider</h2>
+        </div>
+      </Slide>
+    </Carousel>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
-
+import Carousel from '@/components/Carousel.vue'
+import Slide from '@/components/Slide.vue'
 export default {
   name: 'Home',
+  components: {
+    Carousel,
+    Slide,
+  },
 }
 </script>
+
+<style scoped>
+.carousel {
+  max-height: 100vh;
+  height: 100vh;
+}
+</style>
