6385ede79e2d3d1bb0baab0d200c6fcb26c714d4
tal7aouy <talhaouycoding@gmail.com>
2021-11-09 19:44:41 +0100

    initial commit

---
+++ src/components/Carousel.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'Carousel',
+}
+</script>
+
+<template>
+  <div class="carousel">
+    <slot />
+  </div>
+</template>
---
+++ src/components/Slide.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'Slide',
+}
+</script>
+<template>
+  <div class="slide">
+    <transition name="slide">
+      <slot />
+    </transition>
+  </div>
+</template>
